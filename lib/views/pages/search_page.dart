dcad8ccf56c97ccde3caf04734b3b979c206c7b7
eustachekamala <eustachejared25@gmail.com>
2025-07-01 13:49:57 +0300

    updated the search page

--- lib/views/pages/search_page.dart
+++ lib/views/pages/search_page.dart
@@ -8,19 +8,108 @@ class SearchPage extends StatefulWidget{
 }
 
 class _SearchPageState extends State<SearchPage>{
+  TextEditingController searchController = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0;
+  String? menuItems = 'Item 1';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(
-              width: 400,
-              child: SearchBar(
-                backgroundColor: WidgetStateProperty.all(Colors.grey[800]),
-                hintText: 'Search...',
-                leading: Icon(Icons.search),
+            DropdownButton(
+                value: menuItems,
+                hint: Text('Select an item'),
+                items: [
+                  DropdownMenuItem(
+                    value: 'Item 1',
+                    child: Text('Item 1'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Item 2',
+                    child: Text('Item 2'),
+                  ),
+                ],
+                onChanged: (String? value) {
+                  setState(() {
+                    menuItems = value;
+                  });
+                }
               ),
+            TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            Text(searchController.text),
+
+            CheckboxListTile.adaptive(
+              tristate: true,
+              title: Text('Select'),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+
+            SwitchListTile.adaptive(
+              title: Text('Switch me'),
+              value: isSwitched,
+              onChanged: (bool value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },),
+
+            Slider.adaptive(
+              value: sliderValue,
+              divisions: 10,
+              onChanged: (double value) {
+              setState(() {
+                sliderValue = value;
+              });
+            },),
+
+            InkWell(
+              onTap: () {
+                print('Image tapped');
+              },
+                child: Container(
+                    height: 600,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/Kids.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                )
+            ),
+            FilledButton.icon(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                print('Button tapped');
+              },
+              icon: Icon(Icons.add),
+              label: Text('Add'),
             ),
           ],
         ),
